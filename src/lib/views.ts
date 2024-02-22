24ea6372c32af67a01673ae9d04a386c1a27162e
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-22 22:26:35 +0300

    add views module

---
+++ src/lib/views.ts
@@ -0,0 +1,25 @@
+export interface View {
+  title: string;
+  endpoint: string;
+}
+
+export enum ViewType {
+  TRENDING = 'trending',
+  NOW_PLAYING = 'nowPlaying',
+  UPCOMING = 'upComing'
+}
+
+export const views: Record<ViewType, View> = {
+  [ViewType.TRENDING]: {
+    title: 'Trending',
+    endpoint: 'trending/movie/day'
+  },
+  [ViewType.NOW_PLAYING]: {
+    title: 'Now Playing',
+    endpoint: 'movie/now_playing'
+  },
+  [ViewType.UPCOMING]: {
+    title: 'Upcoming',
+    endpoint: 'movie/upcoming'
+  }
+};

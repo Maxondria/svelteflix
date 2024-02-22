bbbdab8ba3a7191f46b13d37f789f15abea94466
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-22 22:32:05 +0300

    pass view props to the carousel component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+  import Carousel from '$lib/components/Carousel.svelte';
   import Hero from '$lib/components/Hero.svelte';
+  import { views } from '$lib/views.js';
 
   export let data;
 </script>
@@ -7,3 +9,6 @@
 <h1 class="column">Top trending movies</h1>
 
 <Hero movie={data.featured} />
+<Carousel movies={data.trending.results} view={views.trending} href="/movies/trending" />
+<Carousel movies={data.nowPlaying.results} view={views.nowPlaying} href="/movies/now-playing" />
+<Carousel movies={data.upComing.results} view={views.upComing} href="/movies/upcoming" />

8a4b34abebf143fe3acc4a71fc2ce49aa28fbdf9
Maxon Tayebwa <maxtayebwa@gmail.com>
2024-02-23 16:55:46 +0300

    implement client side caching

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,8 @@
 export const baseURL = 'https://api.movies.tastejs.com';
 export const mediaBaseURL = 'https://image.tmdb.org/t/p';
 
+const cache = new Map();
+
 export type GetArgs = {
   fetch: typeof globalThis.fetch;
   endpoint: string;
@@ -10,9 +12,19 @@ export type GetArgs = {
 export const get = async <T>({ fetch, endpoint, params }: GetArgs): Promise<T> => {
   const q = new URLSearchParams(params);
 
-  const response = await fetch(`${baseURL}/${endpoint}?${q}`);
+  const url = `${baseURL}/${endpoint}?${q}`;
 
-  return (await response.json()) as T;
+  if (cache.has(url)) {
+    return cache.get(url) as T;
+  }
+
+  const response = await fetch(url);
+
+  const data = (await response.json()) as T;
+
+  cache.set(url, data);
+
+  return data;
 };
 
 export const media = ({ filePath, width }: { filePath: string; width: number }) => {
